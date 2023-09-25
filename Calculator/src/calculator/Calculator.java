9bc693cfb321e463c1721916c47550911dc44cd3
Alexiane Feuillet <alexiane.feuillet@epfedu.fr>
2023-09-25 20:58:31 +0200

    CALCULATOR

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -26,14 +26,52 @@ public class Calculator {
         // TODO code application logic here
         int operateur;
         Scanner sc = new Scanner(System.in);
-        System.out.println("\n Entrer le nombre :");
+        System.out.println("\n Entrez un nombre (Partie 2 du tp, inutile pour la suite)  :");
         operateur =sc.nextInt(); 
-        System.out.println("Please enter the operator:\n" +
+        System.out.println("Please enter the operator:\n" + 
         "1) add \n" +
         "2) substract\n" +
         "3) multiply\n" +
         "4) divide\n" +
-        "5) modulo\n");           
+        "5) modulo\n");     
+        operateur = sc.nextInt(); // On demande a sc de donner le prochain entier
+        if (operateur<1 || operateur>5) {
+            System.out.println("Erreur, vous devez choisir un operateur entre 1 et 5 ");
+            System.exit(0);
+            
+        }
+        int operande1;
+        int operande2;
+
+        System.out.println("Veuillez entrer une premiere valeur");
+        operande1 = sc.nextInt();
+        System.out.println("Maintenant entrez une seconde valeur");
+        operande2 = sc.nextInt();
+
+        int result = 0;
+        switch (operateur) {
+            case 1:
+                result = operande1 + operande2;
+                break;
+            case 2:
+                result = operande1 - operande2;
+                break;
+            case 3:
+                result = operande1 * operande2;
+                break;
+            case 4:
+                result = operande1 / operande2;
+                break;
+            case 5:
+                result = operande1 % operande2;
+                break;
+            default:
+                System.out.println("Erreur, vous n'avez pas choisi un operateur entre 1 et 5!");
+                break;
+        }
+        if (operateur >= 1 && operateur <= 5) {
+            System.out.println("Le resultat est " + result);
+        }
     }
     
 }
