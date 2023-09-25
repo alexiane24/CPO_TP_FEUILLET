c294b6512d0b9bc24b44b4496091b55e7ca44304
Alexiane Feuillet <alexiane.feuillet@epfedu.fr>
2023-09-25 15:30:23 +0200

    exo 1

---
+++ Exo1/src/exo1/Exo1.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ *
+/* TP0 Exercice 1
+* FEUILLET Alexiane
+* FGE2
+* TP DE FAMILIARISATION AVEC NETBEANS
+* 25.09.2023
+*/
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alexi
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Bonjour");
+        System.out.println("Au revoir"); 
+        
+        String prenom;
+        Scanner sc; 
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour, avez-vous passé une bonne journée? Si la réponse est non, bril.");
+        prenom = sc.nextLine();
+    }
+    
+}
